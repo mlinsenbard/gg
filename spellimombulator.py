ec2f39b1e1614fa02b6e9548807d63c48bce56fe
Max Linsenbard <Vex@Maxs-MacBook-Air.local>
2014-11-07 02:24:40 -0800

    Trying to parse abilities... its hard

--- spellimombulator.py
+++ spellimombulator.py
@@ -12,9 +12,9 @@ NEGATIVE = ['n','no','No','N','Nope','nope']
 # Riot Games API scraping
 # Obtains all Champion data for use in functions
 result = urlopen(
-	"https://na.api.pvp.net/api/lol/na/v1.2/champion?api_key=84a61ad2-30ec-4067-b06a-2c6cea9fa344")
-championList = ast.literal_eval(result.read().replace('false', 'False').replace('true','True'))
-championIDList = [x['id'] for x in championList['champions']]
+	"https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?locale=en_US&champData=spells&api_key=84a61ad2-30ec-4067-b06a-2c6cea9fa344")
+championData = ast.literal_eval(result.read().replace('false', 'False').replace('true','True'))
+championList = championList['data']
 
 
 #############
@@ -36,6 +36,37 @@ def printIntroText():
 	print "Then, via magical donger technology, calculates the most 'efficient' spell between all champions."
 
 #############################
+# getInputValues
+#
+# Input:
+# Output: a 3-tuple of AD, AP, and CDR values 
+# 
+# Obtains and sanitizes user input
+#############################
+def getInputValues():
+	ad = raw_input('\nEnter an integer for your desired AD: ')
+	while any(char.isalpha() for c in ad) or ad < 0:
+		ad = raw_input('\nPlease enter a positive integer for your AD: ')
+	ap = raw_input('\nEnter an integer for your desired AP: ')
+	while any(char.isalpha() for c in ap) or ap < 0:
+		ap = raw_input('\nPlease enter a positive integer for your AP: ')
+	cdr = raw_input('\nEnter a value from 0 to 100 representing your CDR: ')
+	while any(char.isalpha() for c in cdr) or cdr < 0 or cdr > 100:
+		cdr = raw_input('\nPlease enter an integer in the range 0-100 representing your CDR: ')
+	return (ad,ap,cdr)
+
+#############################
+# smMostEfficient
+#
+# Input: a 3-tuple of AD,AP,CDR
+# Output: A dictionary representing a LoL spell
+# 
+# given ad, ap, and cdr, finds the most 'efficient' champion spell in the game
+#############################
+def smMostEfficient(dataTuple):
+	pass
+
+#############################
 # smRunner
 #
 # Input:
@@ -50,7 +81,8 @@ def smRunner():
 
 	while userInput not in NEGATIVE:
 		if userInput in AFFIRMATIVE:
-			# DO the thing, get the input, yay!
+			variableTuple = getInputValues()
+			spell = smMostEfficient(variableTuple)
 			userInput = raw_input('Once more? With pizzaz!? (y/n): ')
 		else:
 			userInput = raw_input('... Can you try answering "yes" or "no", summoner?: ')
